7ce9d239482e9de6460d60be761b563b3998070f
Zedritsch <cedric.buttkus@gmail.com>
2024-03-11 17:34:39 +0100

    Update configuration for `C`

--- c/make.bat
+++ c/make.bat
@@ -1,9 +1,20 @@
 @echo off
 
+set retry=0
+
 if not exist bin mkdir bin
 
 pushd bin
 
-cl -Fe..\program.exe -I..\lib ..\src\**
+:compile
+cl -Fe..\program.exe -I..\lib ..\src\** 2>NUL
+
+if %retry%==0 if %errorlevel%==9009 (
+	set retry=1
+
+	call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvarsall.bat" x64
+
+	goto compile
+)
 
 popd
